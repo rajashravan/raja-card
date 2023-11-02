a4e927e11dad0ef49a2800a5ac91e44d36d62dfb
Raja Shravan <raja@banter.shopping>
2023-11-02 12:29:15 -0400

    Initial Commit

---
+++ RajaCard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RajaCard
+//
+//  Created by Raja Shravan on 2023-11-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RajaCard/RajaCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  RajaCardApp.swift
+//  RajaCard
+//
+//  Created by Raja Shravan on 2023-11-02.
+//
+
+import SwiftUI
+
+@main
+struct RajaCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
